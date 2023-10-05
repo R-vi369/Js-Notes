3e2cab0574b6b395ed6343bf18f2d13621847118
ravi <sama369.rk@gmail.com>
2023-10-05 21:09:11 +0530

    arrays

---
+++ js/Arrays.js
@@ -0,0 +1,37 @@
+// array is a variable use to store multiple values at a time
+  
+// starts with 0 index
+  const lang = ['js', 'py', 'c'];
+  console.log(lang[1]);
+
+
+//   Push - this method will add html in the end of the array
+
+  lang.push('html');
+  console.log(lang);
+
+
+    // unshift - add value in the initial state
+  lang.unshift('tailwind');
+  console.log(lang);
+
+//    pop - remove the value from end
+  lang.pop();
+  console.log(lang);
+//   console.log(lang.pop()); also show the value 
+
+//    shift - remove value from start
+lang.shift();
+  console.log(lang);
+
+
+// ********************************
+
+
+
+
+
+
+
+
+
