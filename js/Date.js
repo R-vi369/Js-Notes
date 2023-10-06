cd27729ef1ebfc547071701e7e8fc161cd9fbaa8
ravi <sama369.rk@gmail.com>
2023-10-06 10:30:19 +0530

    dates

---
+++ js/Date.js
@@ -0,0 +1,22 @@
+let date = new Date();
+console.log(date);
+//  console.log(date.toString());
+//  console.log(date.toDateString());
+//  console.log(date.toLocaleString());
+//  console.log(typeof date);
+
+ let hour = date.getHours()
+ let minutes = date.getMinutes()
+ let secn = date.getSeconds()
+  
+    const timeset =()=>{
+
+        const time = `${hour}: ${minutes} : ${secn}`
+
+    }
+
+
+  console.log(time);
+
+
+  
