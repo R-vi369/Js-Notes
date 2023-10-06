a51a9a73c03690f945d59609117e6c890ab7b078
ravi <sama369.rk@gmail.com>
2023-10-06 09:57:48 +0530

    Strings

---
+++ js/String.js
@@ -0,0 +1,31 @@
+// Concatenation
+const name = 'ravi'
+const num = 20;
+console.log(name + num);
+
+console.log(`my name is ${name} and my age is ${num}`);
+
+
+
+  const animal = 'elephant';
+  console.log(animal.length);
+
+  console.log(animal.toUpperCase());
+
+  console.log(animal.indexOf('t'));
+
+  console.log(animal.substring(0, 5));
+
+
+
+  console.log(animal.slice(3, 7));
+
+
+  
+//   check this two on google
+  //   trim and replace
+
+
+
+
+
